009a740c974b501a9afa6b7330e4e706d721cb13
İSMAİL AKBUDAK <isoakbudak@gmail.com>
2014-08-17 19:35:16 +0300

    Article validation and update process finished -Article update process -Article validations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,18 +5,39 @@ class ArticlesController < ApplicationController
     end
 
 	def new
+		 @article = Article.new
 	end
     
     def create
     	@article = Article.new( article_params )
  
-        @article.save
-        redirect_to @article
-    	
+        if @article.save
+           redirect_to @article
+    	else
+    		render 'new'
+    	end
     	## Sayfaya post verilerini basar
     	# render plain: params[:article].inspect
 	end
     
+    def edit
+       @article = Article.find(params[:id])
+       #unless @article.present?
+       #	 redirect_to root_path
+       #end
+
+    end
+
+    def update
+	  @article = Article.find(params[:id])
+	 
+	  if @article.update(article_params)
+	    redirect_to @article
+	  else
+	    render 'edit'
+	  end
+	end
+
     def show
     	 @article = Article.find(params[:id])
     end
