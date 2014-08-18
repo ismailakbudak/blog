a33797508c7ed43e3fe1e0da70cd0cbecaf2d4af
İSMAİL AKBUDAK <isoakbudak@gmail.com>
2014-08-18 10:11:10 +0300

    Revert "Article delete was added"
    This reverts commit d94ee24f7f5acd4e426b0596fef49e36407d3971.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -37,13 +37,6 @@ class ArticlesController < ApplicationController
 	    render 'edit'
 	  end
 	end
-    
-    def destroy
-	  @article = Article.find(params[:id])
-	  @article.destroy
-	 
-	  redirect_to articles_path
-	end
 
     def show
     	 @article = Article.find(params[:id])
