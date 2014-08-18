4e4cef2a55b9d7b658c1f3676d7cfff035a44b77
İSMAİL AKBUDAK <isoakbudak@gmail.com>
2014-08-18 10:26:44 +0300

    Added static pages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
+  
   def index
   end
+  
+  def help
+  end
+
+  def about
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get 'welcome/index'
+  get 'welcome/help'
+  get 'welcome/about'
   root 'welcome#index' 
   resources :articles 
   
